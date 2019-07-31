02fb2094126898c5d79052a53fd4eaa779cc55c2
Kobayashi-takumi <takumi.kobayashi.development@gmail.com>
2019-07-31 09:00:37 +0900

    create tag

--- src/components/ContentsDetail.vue
+++ src/components/ContentsDetail.vue
@@ -1,33 +1,56 @@
 <template>
     <div id="contents-detail">
+
         <div class="container" v-if="writeMode">
             <i class="material-icons" @click="isDeactive">clear</i>
             <input type="text" class="forms" v-model="detailData.title">
             <textarea cols="30" rows="10" class="forms" v-model="detailData.contents"></textarea>
-            <input type="text" class="forms">
+            <div class="detail-tags">
+                    <div v-for="(tag, index) in detailData.tags" :key="index" class="tag">
+                        {{ tag }}
+                        <i class="material-icons" @click="tagClear(index)">clear</i>
+                    </div>
+                    <div class="tag new-tag" v-if="tagAddMode" >
+                        <input type="text" v-model='tag' @blur='tagAddModeOff' @keydown.enter="tagAdd(); tagAddModeOff();">
+                    </div>
+                    <div class="tag new-tag" v-else>
+                        new
+                        <i class="material-icons" @click="tagAddModeOn">create</i>
+                    </div>
+            </div>
             <div class="forms btn">
                 <button @click="writeModeOn">Update</button>
             </div>
         </div>
+
         <div class="container" v-else>
             <i class="material-icons" @click="isDeactive">clear</i>
             <div class="views">
                 <p>{{ detailData.title }}</p>
                 <p>{{ detailData.contents }}</p>
+                <div class="detail-tags">
+                    <div>tag:</div>
+                    <div v-for="(tag, index) in detailData.tags" :key="index" class="tag">{{ tag }}</div>
+                </div>
             </div>
             <div class="forms btn">
                 <i class="material-icons" @click="writeModeOn">create</i>
             </div>
         </div>
+
     </div>
 </template>
 
 <script>
+/* eslint-disable  no-console*/
+
 export default {
     name: 'contents-detail',
     data() {
         return {
             writeMode: false,
+            tagAddMode: false,
+            tag: '',
         }
     },
     methods: {
@@ -36,6 +59,19 @@ export default {
         },
         writeModeOn() {
             this.writeMode = !this.writeMode;
+        },
+        tagAddModeOn() {
+            this.tagAddMode =  true;
+        },
+        tagAddModeOff() {
+            this.tagAddMode = false;
+        },
+        tagAdd() {
+            this.detailData.tags.push(this.tag);
+            this.tag = '';
+        },
+        tagClear(id) {
+            this.detailData.tags.splice(id, 1);
         }
     },
     computed: {
@@ -73,5 +109,12 @@ export default {
 .btn {
     display: flex;
     justify-content: flex-end;
+}
+
+.detail-tags {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: auto;
 }
 </style>
--- src/components/ContentsView.vue
+++ src/components/ContentsView.vue
@@ -4,6 +4,10 @@
             <div v-for='(item, index) in items' :key="index" class="item">
                 <p>{{ item.title }}</p>
                 <p>{{ item.contents }}</p>
+                <div class="tags">
+                    <div>tag:</div>
+                    <div v-for='(tag, index) in item.tags' :key='index' class="tag">{{ tag }}</div>
+                </div>
                 <div class="icons">
                     <i class="material-icons" @click="detailMemo(items[index])">announcement</i>
                     <i class="material-icons" @click="deleteMemo(index)">delete_outline</i>
@@ -24,10 +28,10 @@ export default {
     data() {
         return {
             items: [
-                {title: 'test1', contents: 'content1'},
-                {title: 'test2', contents: 'content2'},
-                {title: 'test3', contents: 'content3'},
-                {title: 'test4', contents: 'content4'},
+                {title: 'test1', contents: 'content1', tags: ['python', 'javascript']},
+                {title: 'test2', contents: 'content2', tags: ['python', 'javascript']},
+                {title: 'test3', contents: 'content3', tags: ['python', 'javascript']},
+                {title: 'test4', contents: 'content4', tags: ['python', 'javascript']},
             ],
             writeMode: false,
         }
@@ -69,7 +73,6 @@ export default {
 }
 
 .icons {
-    margin-top: auto; 
     display: flex;
     justify-content: flex-end;
     align-items: flex-end;
@@ -77,5 +80,19 @@ export default {
 
 .icons i {
     margin-left: 10px;
+}
+
+.tags {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    margin-top: auto;
+}
+
+.tag {
+        margin: 5px;
+        border: 1px solid;
+        border-radius: 10px;
+        padding: 1px;
 }
 </style>
